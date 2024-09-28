73c09667b42e2ce48e39e7d14b6d2df6d83970d5
Austin Kim <40729751+austinkimchi@users.noreply.github.com>
2024-09-28 01:51:57 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+    port: 587,
+    host: "live.smtp.mailtrap.io",
+    auth: {
+        user: process.env.MAILTRAP_USERNAME,
+        pass: process.env.MAILTRAP_PASSWORD,
+    },
+    secure: false,
+    tls: {
+        ciphers: 'SSLv3'
+    }
+});
+
+app.use('/js', express.static(path.join(__dirname, 'public/js')));
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+
+app.use('/about', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/about.html'));
+});
+
+app.use('/contact', async (req, res) => {
+    // check if the request is POST
+    if (req.method === 'POST') {
+        // get the form data
+        let data = '';
+        req.on('data', chunk => {
+            data += chunk.toString();
+        });
+        req.on('end', () => {
+            // parse the form data
+            const formData = new URLSearchParams(data);
+            // get the name and email
+            const name = formData.get('name');
+            const email = formData.get('email');
+            const subject = formData.get('subject');
+            const message = formData.get('message');
+
+            // send the email
+            transporter.sendMail({
+                from: process.env.FROMEMAIL,
+                to: process.env.SENDEMAIL,
+                replyTo: email,
+                subject: `ENGR110.austin.kim - ${subject}`,
+                text: `Name: ${name}\nEmail: ${email}\n\n${message}`
+            }, (error, info) => {
+                if (error) {
+                    console.log(error);
+                    res.status(500);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                    res.status(200);
+                }
+            }
+            );
+
+            res.status(202);
+        });
+    }
+
+    res.sendFile(path.join(__dirname, 'public/contact.html'));
+});
+
+app.use('/project', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/project.html'));
+});
+
+app.use('/', async (req, res) => {
+    if (req.url === '/' || req.url === '/home')
+        res.sendFile(path.join(__dirname, 'public/home.html'));
+    else
+        res.sendFile(path.join(__dirname, 'public/404.html'));
+});
+
+app.use(async (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/404.html'));
+});
+
+
+var port = process.env.PORT || 3000;
+
+if (process.env.MODE && process.env.MODE.toLowerCase() == 'prod')
+    port = process.env.PRODPORT;
+else if (process.env.MODE && process.env.MODE.toLowerCase() == 'dev')
+    port = process.env.DEVPORT;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
