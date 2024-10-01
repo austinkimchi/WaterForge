e92cacf0120a5124c9d7961dd10d9c6eab43c669
Austin Kim <40729751+austinkimchi@users.noreply.github.com>
2024-10-01 13:07:22 -0700

    debg

--- main.js
+++ main.js
@@ -49,6 +49,7 @@ app.use('/contact', async (req, res) => {
             const subject = formData.get('subject');
             const message = formData.get('message');
             const grecaptcha = formData.get('g-recaptcha-response');
+            console.log(grecaptcha)
             if (grecaptcha == "") {
                 res.status(400);
                 return res.send('Please fill out all the fields');
